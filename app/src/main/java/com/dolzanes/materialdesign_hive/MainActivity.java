0834c6ad5c805ee6d14f30d9a778a65383725490
fdolzanes1 <fdolzanes1@gmail.com>
2016-12-14 23:58:13 -0400

    implemeting navigationdrawer item selection

--- app/src/main/java/com/dolzanes/materialdesign_hive/MainActivity.java
+++ app/src/main/java/com/dolzanes/materialdesign_hive/MainActivity.java
@@ -1,5 +1,8 @@
 package com.dolzanes.materialdesign_hive;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,10 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.dolzanes.materialdesign_hive.activity.FragmentDrawer;
+import com.dolzanes.materialdesign_hive.activity.FriendsFragment;
+import com.dolzanes.materialdesign_hive.activity.HomeFragment;
+import com.dolzanes.materialdesign_hive.activity.MessagesFragment;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
+
+    private static String TAG = MainActivity.class.getSimpleName();
 
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
@@ -29,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+
+        displayView(0);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,13 +49,51 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
 
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
+        if(id == R.id.action_search) {
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
         if(id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "Settings action is selected!", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
 
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new FriendsFragment();
+                title = getString(R.string.title_friends);
+                break;
+            case 2:
+                fragment = new MessagesFragment();
+                title = getString(R.string.title_messages);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
---
+++ app/src/main/java/com/dolzanes/materialdesign_hive/activity/FriendsFragment.java
@@ -0,0 +1,50 @@
+package com.dolzanes.materialdesign_hive.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dolzanes.materialdesign_hive.R;
+
+/**
+ * Created by dolzanes on 14/12/16.
+ */
+
+public class FriendsFragment  extends Fragment {
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Checking the fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
